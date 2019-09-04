7aec31c78da41e7c9b62abf49eab5809be72b65a
Javier Chavez <54275602+JavierChavez-UTEC@users.noreply.github.com>
2019-09-04 16:56:29 -0500

    Ejercicio 6 arreglado.

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+	float quantity, temporal, media, suma = 0, sumaAlpha = 0;
+	cout << "N: ";
+	cin >> quantity;
+	for (int i = 0; i < quantity; i++) {
+		cout << 'x' << i + 1 << ": ";
+		cin >> temporal;
+		suma += temporal;
+		sumaAlpha += pow(temporal, 2);
+	}
+	cout << "sd: " << fixed << setprecision(2) << sqrt(sumaAlpha / quantity - pow(suma / quantity, 2));
 }
