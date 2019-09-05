d1eddc460a87c2db855e09aae018ee9d53e7a561
Javier Chavez <54275602+JavierChavez-UTEC@users.noreply.github.com>
2019-09-04 22:17:55 -0500

    Ejercicio 7 arreglado.

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main() {
-
-    return 0;
+    string numero;
+    cin>>numero;
+    cout<<"SOAT:";
+    if(int(numero[4])%2 == 0)
+        cout<<"Vigente";
+    else
+        cout<<"Vencido";
+    cout<<endl<<"CITV:";
+    if(int(numero[5])%2 == 0)
+        cout<<"Vigente";
+    else
+        cout<<"Vencido";
+    cout<<endl<<"AUTORIZADO:";
+    if(int(numero[6] + numero[7])%3 == 0)
+        cout<<"Si";
+    else
+        cout<<"No";
 }
-
